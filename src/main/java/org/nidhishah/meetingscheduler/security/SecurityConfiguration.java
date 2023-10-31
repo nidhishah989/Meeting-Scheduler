4ae3ceb9c46f728ce1c1127df18e80a0b5f386d9
nidhishah989 <nidhishah989@gmail.com>
2023-10-31 15:40:59 -0700

    add other security bean

--- src/main/java/org/nidhishah/meetingscheduler/security/SecurityConfiguration.java
+++ src/main/java/org/nidhishah/meetingscheduler/security/SecurityConfiguration.java
@@ -1,10 +1,14 @@
 package org.nidhishah.meetingscheduler.security;
 
 import org.nidhishah.meetingscheduler.config.OrganizationFilter;
+import org.nidhishah.meetingscheduler.services.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
@@ -13,6 +17,24 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Autowired
+    private UserServiceImpl userDetailsService;
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+        auth.setUserDetailsService(userDetailsService); //set the custom user details service
+        auth.setPasswordEncoder(passwordEncoder()); //set the password encoder - bcrypt
+        return auth;
+    }
+
+    //beans
+    //bcrypt bean definition
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(11);
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http    .addFilterBefore(new OrganizationFilter(), UsernamePasswordAuthenticationFilter.class)
