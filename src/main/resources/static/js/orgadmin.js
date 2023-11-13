52352ae00b1faf2bae5d86c2565ed59b78bc81ff
nidhishah989 <nidhishah989@gmail.com>
2023-11-13 14:48:33 -0800

    password requirement has set

--- src/main/resources/static/js/orgadmin.js
+++ src/main/resources/static/js/orgadmin.js
@@ -1,97 +1,4 @@
 
-const form = document.querySelector('form');
-  const passwordInput = document.querySelector('#password1');
-  const confirmPasswordInput = document.querySelector('#password2');
-  const passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}$/;
-
- const OpenAccount=(e)=>{
-  e.preventDefault();
-  const password = passwordInput.value;
-  const confirmPassword = confirmPasswordInput.value;
-
-  if (!passwordPattern.test(password)) {
-    e.preventDefault();
-    alert('Password is not valid. It should contain at least 8 characters with a mix of letters, numbers, and special symbols.');
-  }
-
-  if (password !== confirmPassword) {
-    e.preventDefault();
-    alert('Password and Confirm Password do not match.');
-  }
-
-  if (passwordPattern.test(password) && password === confirmPassword) {
-      console.log("Form is valid");
-      // Set the URL of the new page you want to navigate to
-      // const newPageUrl = 'organizationsetup.html';
-      // window.location.href = newPageUrl;
-      const formData = {
-              OrgName:{
-                  orgName: document.querySelector('#organizationName').value,
-                  teammates :{
-                      admin:{
-                          adminUsername: document.querySelector('#adminEmail').value.split('@')[0],
-                          adminEmail: document.querySelector('#adminEmail').value,
-                          adminpassword: document.querySelector('#adminEmail').value,
-                          adminFirstname: document.querySelector('#firstName').value,
-                          adminLastname: document.querySelector('#lastName').value,
-                      }
-                  }
-              }
-          };
-
-      console.log(data);
-      console.log(formData['OrgName']);
-      data ['OrgName']=formData.OrgName;
-      console.log(data);
-      localStorage.setItem('data', JSON.stringify(data));
-      window.location.href = 'organizationsetup.html';
-    }
- }
-  // form.addEventListener('submit', function (e) {
-  //   const password = passwordInput.value;
-  //   const confirmPassword = confirmPasswordInput.value;
-
-  //   if (!passwordPattern.test(password)) {
-  //     e.preventDefault();
-  //     alert('Password is not valid. It should contain at least 8 characters with a mix of letters, numbers, and special symbols.');
-  //   }
-
-  //   if (password !== confirmPassword) {
-  //     e.preventDefault();
-  //     alert('Password and Confirm Password do not match.');
-  //   }
-
-  //   if (passwordPattern.test(password) && password === confirmPassword) {
-  //       console.log("Form is valid");
-  //       // Set the URL of the new page you want to navigate to
-  //       // const newPageUrl = 'organizationsetup.html';
-  //       // window.location.href = newPageUrl;
-  //       const formData = {
-  //               OrgName:{
-  //                   orgName: document.querySelector('#organizationName').value,
-  //                   teammates :{
-  //                       admin:{
-  //                           adminUsername: document.querySelector('#adminEmail').value.split('@')[0],
-  //                           adminEmail: document.querySelector('#adminEmail').value,
-  //                           adminpassword: document.querySelector('#adminEmail').value,
-  //                           adminFirstname: document.querySelector('#firstName').value,
-  //                           adminLastname: document.querySelector('#lastName').value,
-  //                       }
-  //                   }
-  //               }
-  //           };
-
-  //       console.log(data);
-  //       console.log(formData['OrgName']);
-  //       data ['OrgName']=formData.OrgName;
-  //       console.log(data);
-  //       localStorage.setItem('data', JSON.stringify(data));
-
-
-  //     }
-  // });
-
-
   const emailInput = document.querySelector('#adminEmail');
   const emailValidationMessage = document.querySelector('#emailValidationMessage');
 
@@ -105,4 +12,42 @@ const form = document.querySelector('form');
       emailValidationMessage.textContent = 'Required email format- ref example: username@domain.com';
     }
   });
+
+const passwordInput1 = document.getElementById('password1');
+// Function to validate password pattern
+passwordInput1.addEventListener('input', function () {
+    const password = passwordInput1.value;
+    const passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}$/;
+
+    if (passwordPattern.test(password)) {
+        passwordValidationMessage.textContent = ''; // Valid password format
+    } else {
+        passwordValidationMessage.textContent = 'Password should contain at least 8 characters with a mix of letters, numbers, and special symbols.';
+    }
+});
+
+const submitButton = document.getElementById('sign_up_btn');
+
+submitButton.addEventListener('click', function(e) {
+    const passwordInput = document.getElementById('password1').value;
+    const confirmPasswordInput = document.getElementById('password2').value;
+    const passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}$/;
+
+    if (!passwordPattern.test(passwordInput)) {
+        alert('Password is not valid. It should contain at least 8 characters with a mix of letters, numbers, and special symbols.');
+        e.preventDefault();
+        return false;
+    }
+
+    if (passwordInput !== confirmPasswordInput) {
+        alert('Password and Confirm Password do not match.');
+        e.preventDefault();
+        return false;
+    }
+
+    console.log("Event submitted");
+});
+
+
+
 
